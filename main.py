0a8d3920e4e1201c49f1ad25226860ea4a3710fd
Jiří Rypl <jirkarypl2@gmail.com>
2024-11-20 14:18:58 +0100

    create Procfile and .env

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ from forms import RegisterForm, LoginForm, CreatePostForm, CommentForm
 import smtplib
 from time import sleep
 import requests
+import os
+from dotenv import load_dotenv
+import gunicorn
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -28,8 +33,8 @@ login_manager.init_app(app)
 def load_user(user_id):
     return db.get_or_404(User, user_id)
 
-my_email="jirkarypl2@gmail.com"
-my_password="ysbo hhla uoei owhc"
+my_email=os.environ.get("MY_EMAIL")
+my_password=os.environ.get("PASSWORD")
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
@@ -269,4 +274,4 @@ def send_email(name,email,phone,message):
     return render_template("contact.html", msg_sent=True,current_user=current_user)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
